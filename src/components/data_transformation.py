e2caae593630100aaf8712127a305585ba8d1e34
Simran <simran11singh2004@gmail.com>
2023-12-22 00:18:41 +0530

    Hyperparameter tuning

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -24,7 +24,7 @@ class DataTransformation:
 
     def get_data_transformer_object(self):
         '''
-        This function si responsible for data trnasformation
+        This function is responsible for data trnasformation
         
         '''
         try:
@@ -123,4 +123,4 @@ class DataTransformation:
         except Exception as e:
             raise CustomException(e,sys)
         
-        
+
